8bb84a715d94444fb555dc86341699ae1be5bc21
Tyler D. Franklin <irishlep84@gmail.com>
2017-03-07 22:04:42 -0600

    Checkpoint 9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,31 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName("point");
 
-    var points = document.getElementsByClassName("point");
+var animatePoints = function(points) {
 
     var revealPoint = function(index) {
-        points[index].style.opactity = 1;
+        points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
         points[index].style.msTransform = "scaleX(1) translateY(0)";
         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+    };
     
     for (var i = 0; i < points.length; i++) {
         revealPoint(i);
     }
 };
+
+window.onload = function() {
+    console.log(pointsArray.length);
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ };
