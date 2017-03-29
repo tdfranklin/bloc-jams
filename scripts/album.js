27c771156e4669bc745bde641b61486b690376bc
Tyler D. Franklin <irishlep84@gmail.com>
2017-03-28 20:57:42 -0500

    Checkpoint 20 completed

--- scripts/album.js
+++ scripts/album.js
@@ -16,14 +16,21 @@ var createSongRow = function(songNumber, songName, songLength) {
              var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
              currentlyPlayingCell.html(currentlyPlayingSongNumber);
          }
-         if (currentlyPlayingSongNumber !== songNumber) {
-             $(this).html(pauseButtonTemplate);
-             setSong(songNumber);             
+         if (currentlyPlayingSongNumber !== songNumber) {             
+             setSong(songNumber);
+             currentSoundFile.play();
+             $(this).html(pauseButtonTemplate);             
              updatePlayerBarSong();
          } else if (currentlyPlayingSongNumber === songNumber) {
-             $(this).html(playButtonTemplate);
-             $('.main-controls .play-pause').html(playerBarPlayButton);
-             setSong(null);
+             if (currentSoundFile.isPaused()) {                 
+                 $(this).html(pauseButtonTemplate);
+                 $('.main-controls .play-pause').html(playerBarPauseButton);
+                 currentSoundFile.play();                 
+             } else {                 
+                 $(this).html(playButtonTemplate);
+                 $('.main-controls .play-pause').html(playerBarPlayButton);
+                 currentSoundFile.pause();
+             }            
          }
      };
 
@@ -93,6 +100,7 @@ var createSongRow = function(songNumber, songName, songLength) {
      var lastSongNumber = currentlyPlayingSongNumber;
 
      setSong(currentSongIndex + 1);
+     currentSoundFile.play();
 
      updatePlayerBarSong();
 
@@ -114,6 +122,7 @@ var createSongRow = function(songNumber, songName, songLength) {
      var lastSongNumber = currentlyPlayingSongNumber;
 
      setSong(currentSongIndex + 1);
+     currentSoundFile.play();
 
      updatePlayerBarSong();
 
@@ -127,8 +136,24 @@ var createSongRow = function(songNumber, songName, songLength) {
  };
 
  var setSong = function(songNumber) {
+     if (currentSoundFile) {
+         currentSoundFile.stop();
+     }
+
      currentlyPlayingSongNumber = parseInt(songNumber);
      currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+     currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+         formats: [ 'mp3' ],
+         preload: true
+     });
+
+     setVolume(currentVolume);
+ };
+
+ var setVolume = function(volume) {
+     if (currentSoundFile) {
+         currentSoundFile.setVolume(volume);
+     }
  };
 
  var getSongNumberCell = function(number){
@@ -143,6 +168,8 @@ var createSongRow = function(songNumber, songName, songLength) {
  var currentAlbum = null;
  var currentlyPlayingSongNumber = null;
  var currentSongFromAlbum = null;
+ var currentSoundFile = null;
+ var currentVolume = 80;
 
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
